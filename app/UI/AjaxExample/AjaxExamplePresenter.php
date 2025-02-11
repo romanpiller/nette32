388ea2b4ce7163abc2f131b9e5134f118cce3a89
Roman Piller <roman.piller@sportisimo.com>
2025-02-11 13:25:15 +0100

    ajax priklad

--- app/UI/AjaxExample/AjaxExamplePresenter.php
+++ app/UI/AjaxExample/AjaxExamplePresenter.php
@@ -13,7 +13,7 @@ final class AjaxExamplePresenter extends Presenter
 {
   public function handleGenerateNumber01(): void
   {
-    bdump($this->isAjax(), 'handleGenerateNumber01');
+    bdump($this->isAjax(), 'signal GenerateNumber01');
 
     // toto sa nesmie volat ak ajax
     // handle reditectom automaticky nekonci
@@ -21,17 +21,22 @@ final class AjaxExamplePresenter extends Presenter
     // $this->redirect('this');
   }
 
+
   public function handleUpdateMessage(string $snippet, string $message): void
   {
-    bdump($this->isAjax(), 'handleGenerateNumber01');
+    bdump($this->isAjax(), 'signal UpdateMessage');
     $this->template->message = $message;
     $this->redrawControl($snippet);
-    // vola aj renderDefault ale ajaxovy
+    // vola aj renderDefault ale ajaxovy a tym padom konci na detault.latte
+
+    // volam sice renderSignal , takze data spracujem inak ako renderDefault ale renderSignal rovnako vykresluje
+    $this->setView('signal');
   }
+
 
   public function handleGenerateNumber(string $message = ''): void
   {
-    bdump($this->isAjax(), 'handleGenerateNumber');
+    bdump($this->isAjax(), 'signal GenerateNumber');
     $this->sendJson([
         'number' => random_int(1, 100),
         'message' => $message,
@@ -41,12 +46,22 @@ final class AjaxExamplePresenter extends Presenter
 
   public function renderDefault(): void
   {
-    bdump($this->isAjax(), 'renderDefault');
+    bdump($this->isAjax(), 'render - Default');
 
     // inou cestou vyplnas $header potom musis
     if(!isset($this->template->message))
     {
+      bdump($this->isAjax(), 'render - Default - Empty message');
       $this->template->message = 'RESET';
     }
+  }
+
+  public function renderSignal(): void
+  {
+    bdump($this->isAjax(), 'signal Render');
+
+    // inak spracujes data ako v default view ale vykreslovanie je zhodne ak pre signal aj pre default
+
+    $this->template->setFile(__DIR__ . '/default.latte');
   }
 }
